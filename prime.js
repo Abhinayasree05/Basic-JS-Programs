dde6d491d08ebf7d8ab00bf6d77247186c94d559
Abhinayasree <namaniabhinaya@gmail.com>
2026-05-04 22:25:10 +0530

    updated  program

--- prime.js
+++ prime.js
@@ -1,22 +1,20 @@
-function isPrime(n) {
-    if (n <= 1) {
-        return false; // 0 and 1 are not prime
+function checkPrime(num) {
+
+    if (num <= 1) {
+        console.log(num + " is Not Prime");
+        return;
     }
 
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) {
-            return false; // divisible → not prime
+    for (let i = 2; i < num; i++) {
+
+        if (num % i == 0) {
+            console.log(num + " is Not Prime");
+            return;
         }
     }
 
-    return true; // no divisors found → prime
+    console.log(num + " is Prime");
 }
 
-// Example usage
-let num = 7;
+checkPrime(7);
 
-if (isPrime(num)) {
-    console.log(num + " is a Prime Number");
-} else {
-    console.log(num + " is NOT a Prime Number");
-}
